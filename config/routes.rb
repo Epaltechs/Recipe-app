726c43a0c2bf9db8dc251fc159dcdd631839e66f
Emmanuel Paul <emmapaul247@gmail.com>
2022-09-13 15:02:06 +0100

    Add form for displaying list of food and orders

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users do
+    resources :foods
+  end
 
-  root to: "home#index"
+  root 'recipes#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
--- db/migrate/20220912190906_create_food.rb
+++ db/migrate/20220912190906_create_food.rb
@@ -4,6 +4,7 @@ class CreateFood < ActiveRecord::Migration[7.0]
       t.string :name
       t.string :measurement_unit
       t.integer :price
+      t.integer :quantity
       t.integer :user_id
 
       t.timestamps
