201d2ff23568dd07810cbc032a7ccedc37dab70e
Emmanuel Paul <emmapaul247@gmail.com>
2022-09-15 10:02:34 +0100

    Edit shopping list layout

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   end
 
   resources :shopping_lists, only: %i[index]
-  
+
   root 'recipes#index'
   # Defines the root path route ("/")
   # root "articles#index"
