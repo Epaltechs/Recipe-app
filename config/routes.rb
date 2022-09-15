a3f36101ba463364a668e72fdd4d61b3bcce6cc0
Emmanuel Paul <emmapaul247@gmail.com>
2022-09-15 16:45:09 +0100

    Edit route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :users do
     resources :foods
     resources :recipes #only: %i[index new show create]
+    resources :shopping_lists, only: %i[index]
   end
 
   root 'users#index'
