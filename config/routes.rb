51b2d08bec92272b0fae35b8ae26625821d86901
Emmanuel Paul <emmapaul247@gmail.com>
2022-09-15 16:09:10 +0100

    Resolve conflict with dev brance

--- config/routes.rb
+++ config/routes.rb
@@ -5,17 +5,9 @@ Rails.application.routes.draw do
     resources :foods
     resources :recipes #only: %i[index new show create]
   end
-<<<<<<< HEAD
 
-  resources :shopping_lists, only: %i[index]
-
-  root 'recipes#index'
-=======
-  
- 
   root 'users#index'
 
->>>>>>> dev
   # Defines the root path route ("/")
   # root "articles#index"
 end
