c4b7d9ba9d0198d062b6c392a016dc808c874629
Emmanuel Paul <emmapaul247@gmail.com>
2022-09-17 10:31:48 +0100

    Add styling to the app

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,12 @@ Rails.application.routes.draw do
     resources :foods
     resources :recipes, only: %i[index new show create destroy]
   end
-  
- 
+
+  resources :public_recipes, only: %i[index]
+  resources :shopping_lists, only: %i[index]
+  resources :recipe_foods, only: %i[new create destroy]
+
+
   root 'recipes#index'
 
   # Defines the root path route ("/")
